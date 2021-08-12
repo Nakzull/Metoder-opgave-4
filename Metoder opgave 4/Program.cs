a5dfed1b1711acf38890b1d2df1c56b27d323a31
Nakzull <andr25f4@zbc.dk>
2021-08-12 10:44:47 +0200

    Add project files.

---
+++ Metoder opgave 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Metoder_opgave_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber1 = 1;
+            int startNumber2 = 10;
+            CountUp(startNumber1);
+            CountDown(startNumber2);
+            Console.WriteLine("Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            CountUpAndDown(number);
+        }
+        public static void CountUp(int a)
+        {
+            for (int i = a;i<11;i++)
+                Console.WriteLine(i);
+        }
+        public static void CountDown(int a)
+        {
+            for (int i = a; i>0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void CountUpAndDown(int a)
+        {
+            for (int i = a; i <= a + 32; i++)
+                Console.WriteLine(i);
+            for (int i = a; i >= a - 16; i--)
+                Console.WriteLine(i);
+        }
+    }
+}
